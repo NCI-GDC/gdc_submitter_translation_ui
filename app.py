e7d1c318775704f4db6a05f99157733492835e54
Shenglai Li <sli6@uchicago.edu>
2018-02-17 13:22:52 -0600

    feat(upload): upload file and textarea to upload page

--- app.py
+++ app.py
@@ -3,12 +3,10 @@
 flask playground
 """
 
-import os
-from flask import Flask, render_template, request, flash
-#from wtforms import Form, StringField, TextAreaField, validators
-from werkzeug.utils import secure_filename
+import json
+from flask import Flask, render_template, request, flash, redirect, url_for
+from wtforms import Form, TextAreaField, validators
 
-ALLOWED_EXTENSIONS = set(['csv', 'json'])
 APP = Flask(__name__)
 
 @APP.route('/about')
@@ -18,30 +16,51 @@ def about():
     """
     return render_template('about.html')
 
-def allowed_file(filename):
+def file_extention(filename):
     """
     check ext
     """
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower()
+
+class InputForm(Form):
+    """
+    form
+    """
+    inputform = TextAreaField('Or write your input here', [validators.length(min=30)])
 
 @APP.route('/', methods=['GET', 'POST'])
 def home():
     """
     home
     """
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        content = str(form['inputform']).rsplit('>')[1].rsplit('<')[0]
+        flash('File uploaded', 'success')
+        return redirect(url_for('upload', content=content))
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('No file part', 'danger')
-            return render_template('home.html')
+            return render_template('home.html', form=form)
         file = request.files['file']
-        if file.filename == '' or not allowed_file(file.filename):
-            flash('Not a valid file format', 'danger')
-            return render_template('home.html')
-        if file and allowed_file(file.filename):
-            content = file.read()
-            flash('File uploaded', 'success')
-            print(content)
-    return render_template('home.html')
+        if file:
+            if file_extention(file.filename) == 'json':
+                content = json.load(file)
+                flash('File uploaded', 'success')
+                return redirect(url_for('upload', content=content))
+            elif file_extention(file.filename) == 'csv':
+                content = file.read()
+                flash('File uploaded', 'success')
+                return redirect(url_for('upload', content=content))
+            else:
+                flash('Not a valid file format', 'danger')
+    return render_template('home.html', form=form)
+
+@APP.route('/upload/<content>')
+def upload(content):
+    """
+    upload
+    """
+    return render_template('upload.html', content=content)
 
 if __name__ == '__main__':
     APP.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
