5c98b7465d47d07becd3549e3fd9f63facf6b414
Shenglai Li <sli6@uchicago.edu>
2018-02-22 18:16:37 -0600

    feat(mapping): change to 2 steps, mapping from the api

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ flask playground
 """
 
 import json
+import requests as rq
 from flask import Flask, render_template, request, flash, redirect, url_for
 from wtforms import Form, TextAreaField, validators
-
-APP = Flask(__name__)
-
-@APP.route('/about')
-def about():
-    """
-    about
-    """
-    return render_template('about.html')
 
 def file_extention(filename):
     """
@@ -22,45 +14,55 @@ def file_extention(filename):
     """
     return '.' in filename and filename.rsplit('.', 1)[1].lower()
 
-class InputForm(Form):
-    """
-    form
-    """
-    inputform = TextAreaField('Or write your input here', [validators.length(min=30)])
+APP = Flask(__name__)
 
-@APP.route('/', methods=['GET', 'POST'])
+@APP.route('/about')
+def about():
+    """
+    about page
+    """
+    return render_template('about.html')
+
+@APP.route('/')
 def home():
     """
-    home
+    home page
     """
-    form = InputForm(request.form)
-    if request.method == 'POST' and form.validate():
-        content = str(form['inputform']).rsplit('>')[1].rsplit('<')[0]
-        flash('Content uploaded', 'success')
-        return redirect(url_for('upload', content=content))
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return render_template('home.html', form=form)
-        file = request.files['file']
-        if file:
-            if file_extention(file.filename) == 'json':
-                content = json.load(file)
-                flash('File uploaded', 'success')
-                return redirect(url_for('upload', content=content))
-            elif file_extention(file.filename) == 'csv':
-                content = file.read()
-                flash('File uploaded', 'success')
-                return redirect(url_for('upload', content=content))
-            else:
-                flash('Not a valid file format', 'danger')
-    return render_template('home.html', form=form)
+    return render_template('home.html')
 
-@APP.route('/upload/<content>')
-def upload(content):
+@APP.route('/upload', methods=['GET', 'POST'])
+def upload():
     """
-    upload
+    step 1
     """
-    return render_template('upload.html', content=content)
+    all_node_type = rq.get('https://api.gdc.cancer.gov/v0/submission/_dictionary/_all').json()
+    node_list = []
+    for key, value in all_node_type.items():
+        if not key.startswith('_'):
+            node_list.append(key)
+    return render_template('upload.html', node_list=node_list)
+
+@APP.route('/mapping', methods=['GET', 'POST'])
+def mapping():
+    """
+    step 2
+    """
+    selected_node = str(request.form.get('comp_select'))
+    selected_url = 'https://api.gdc.cancer.gov/v0/submission/_dictionary/{}'.format(selected_node)
+    propertyList = []
+    selected_json = rq.get(selected_url).json()
+    for key, value in selected_json.items():
+        if not key.startswith('$'):
+            propertyList.append(key)
+    file = request.files['uploadFile']
+    if file_extention(file.filename) == 'csv':
+        header = file.read().decode("utf-8").strip().split('\n')[0]
+        flash('File uploaded', 'success')
+    else:
+        flash('Not a valid file format', 'danger')
+        return render_template('mapping.html', selected_node=selected_node, propertyList=["None"], header="None")
+    return render_template('mapping.html', selected_node=selected_node, propertyList=propertyList, header=header)
+
 
 if __name__ == '__main__':
     APP.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
